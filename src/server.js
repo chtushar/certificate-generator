ed5dd172933ee3c37b2a1cae4935d6e122af1360
chtushar <choudharit23@gmail.com>
2020-04-21 01:58:47 +0530

    webpack added

---
+++ src/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const path = require('path')
+
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.get('', function (req, res) {
+
+    let filePath = path.join(__dirname, 'index.html')
+    res.sendFile(filePath);
+})
+
+app.listen(PORT, () => {
+    console.log(`Serving at port ${PORT}`);
+})
