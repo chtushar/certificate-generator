d28058ab68b6f8c08333aad2bc4ae5cd94b2e679
chtushar <choudharit23@gmail.com>
2020-04-20 23:53:25 +0530

    base application ready

--- src/app.js
+++ src/app.js
@@ -1,29 +1,48 @@
 const path = require('path');
 const fs = require('fs');
 const puppeteer = require('puppeteer');
+const hbs = require('handlebars');
+
+const data = require('./database.json');
 
 
-(async function() {
-    try{
-        const browser = await puppeteer.launch();
-        const page = await browser.newPage();
 
-        await page.setContent('<h1>Hello</h1>');
-        await page.emulateMedia('screen');
-        await page.pdf({
-            path: 'src/pdf/mypdf.pdf',
-            format: 'A4',
-            printBackground: true
+
+
+const filePath = path.join(__dirname,'templates','certificate.hbs')
+fs.readFile(filePath,(err, content) => {
+
+    if (!err) {
+        data.forEach( (el) => {
+            let html = hbs.compile(content.toString())({name: el.name})
+            exportPDF(html, `${el.name}`)
         })
-
-
-
-        
-        console.log("done");
-        await browser.close();
-        process.exit();
     }
-    catch(e){
-        console.log(e);
+    else{
+        console.log(err);
     }
-})();
+})
+
+
+
+async function exportPDF(content, nameOfFile) {
+        try{
+            const browser = await puppeteer.launch();
+            const page = await browser.newPage();
+    
+
+            await page.setContent(content);
+            await page.emulateMedia('screen');
+            await page.pdf({
+                path: `src/pdf/${nameOfFile}.pdf`,
+                format: 'A4',
+                printBackground: true
+            })
+            await browser.close();
+            process.exit();
+        }
+        catch(e){
+            console.log('Error: '+e);
+        }
+};
+
