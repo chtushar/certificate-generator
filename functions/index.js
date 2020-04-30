e4c75d45bde2ea178a4b23d996b8bc1722a2f2d3
chtushar <choudharit23@gmail.com>
2020-05-01 00:38:09 +0530

    get all users

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,37 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin')
 const express = require('express')
 const app = express()
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
+const firebaseConfig = {
+    apiKey: "AIzaSyCtm4xycvUlTEhvQkyflHCUOkCZ6QCuPd8",
+    authDomain: "certificate-generator-69a8b.firebaseapp.com",
+    databaseURL: "https://certificate-generator-69a8b.firebaseio.com",
+    projectId: "certificate-generator-69a8b",
+    storageBucket: "certificate-generator-69a8b.appspot.com",
+    messagingSenderId: "934758987690",
+    appId: "1:934758987690:web:7aeab45b12c5dedaef1dd3",
+    measurementId: "G-9TYMG7QLG6"
+};
 
+const firebase = require('firebase')
+firebase.initializeApp(firebaseConfig)
+
+
+const db = admin.firestore()
 app.get('/users', (req, res) => {
-    let ok = 'ok';
-    return res.json({ok})
+    db
+    .collection('users')
+    .get()
+    .then((data) => {
+        let users = []
+        data.forEach(doc => {
+            users.push(doc.data())
+        })
+        return res.json(users)
+    })
+    .catch(err => console.log(err))
 })
 
 
