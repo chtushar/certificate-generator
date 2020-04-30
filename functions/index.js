fb14279cb1a8c445a1f91665f2e0adc00f6fd6cc
chtushar <choudharit23@gmail.com>
2020-05-01 00:44:05 +0530

    hide firebase keys

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin')
 const express = require('express')
+const firebaseConfig = require('./firebaseConfig')
 const app = express()
 admin.initializeApp();
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCtm4xycvUlTEhvQkyflHCUOkCZ6QCuPd8",
-    authDomain: "certificate-generator-69a8b.firebaseapp.com",
-    databaseURL: "https://certificate-generator-69a8b.firebaseio.com",
-    projectId: "certificate-generator-69a8b",
-    storageBucket: "certificate-generator-69a8b.appspot.com",
-    messagingSenderId: "934758987690",
-    appId: "1:934758987690:web:7aeab45b12c5dedaef1dd3",
-    measurementId: "G-9TYMG7QLG6"
-};
+
 
 const firebase = require('firebase')
 firebase.initializeApp(firebaseConfig)
